5732158fd0af395d75818bc908b73449df9a8b89
Arthur Forgeard <arty@pop-os.localdomain>
2023-01-20 15:26:30 +0100

    yep

--- clien.py
+++ clien.py
@@ -1,11 +1,25 @@
 import socket
-pseudo = "default"
+import threading
 
+
+
+
+pseudo = "default"
 host = input("IP:")
 port = int(input("Port:"))
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 pseudo = input("Enter Your Name: ")
+class   update_listener(threading.Thread):
+            def __init__(self, socket):
+                threading.Thread.__init__(self)
+                self.socket = socket
+            def run(self):
+                print("yep gro!")
+                while True:
+                        data = self.socket.recv(1024)
+                        data = data.decode("utf8")
+                        if data:
+                                print(data)
 
 try:
     socket.connect((host, port))
@@ -17,6 +31,8 @@ except ConnectionError:
     print("connection Error.")
     socket.close()
 finally:
+    test = update_listener(socket)
+    test.start()
     while True:
         msg = input("MSG: ")
         msg = msg.encode("utf8")
--- server.py
+++ server.py
@@ -1,22 +1,30 @@
 import socket
 import threading
 
-
+clients=[]
 class ThreadClient(threading.Thread):
-        def __init__(self, conn):
+        def __init__(self, conn, clients):
                 threading.Thread.__init__(self)
                 self.conn = conn
+                self.clients = clients
         def run(self):
                 data = self.conn.recv(16)
                 data = data.decode("utf8")
                 name = data
                 print("<", name, ">", "connected to the server from:", address)
                 print(threading.current_thread())
+                self.clients += [conn]
+                print(self.clients)
                 while True:
                         data = self.conn.recv(1024)
+                        for client in self.clients:
+                                if client != conn:
+                                        msg = name.encode("utf8") + ": " + data
+                                        client.sendall(msg)
                         data = data.decode("utf8")
                         if data == "/deco":
                                 print("<", name, ">", "disconnected from the server")
+                                self.clients -= [conn]
                                 break
                         if data:
                                 print("Neme:", name, "|", data)
@@ -31,7 +39,7 @@ while True:
         socket.listen(2)
         conn, address = socket.accept()
         #print("connection from:", address)
-        my_thread = ThreadClient(conn)
+        my_thread = ThreadClient(conn, clients)
         my_thread.start()
 conn.close()
 socket.close()
